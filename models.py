b46eacbdcb06bdd53796d85978f2f0a4cc3c966f
webcut <webcut@v39245.kvm.u16071>
2019-04-07 19:00:50 +0300

    Before change models.py for save user in separate function

--- models.py
+++ models.py
@@ -1,19 +1,12 @@
 from django.db import models
-import os
 from datetime import datetime, timedelta
+from django.contrib.auth.models import User
+from files import settings as files_settings
 
 # Models
 	
-class User(models.Model):
-	identifier = models.CharField(max_length = 100, unique = False)
-	code = models.CharField(max_length = 100, unique = False)
-	date_time = models.DateTimeField(auto_now = True)
-
-	def __str__(self):
-		return str(self.identifier)
-
 def user_path_file(instance_content, empty_parameter):
-	return 'files/{0}/{1}'.format(instance_content.user.id, instance_content.file.name)
+	return '{0}/{1}/{2}'.format(files_settings.FILES_ROOT, instance_content.user.id, instance_content.file.name)
 
 class Content(models.Model):
 	user = models.ForeignKey(User, on_delete = models.CASCADE)
@@ -22,15 +15,15 @@ class Content(models.Model):
 	date_time_delete = models.DateTimeField(default = datetime.now() + timedelta(days = 5))
 
 	def __str__(self):
-		return str(self.user)
+		return str(self.file.name)
 
 # Functions
 
 def save_user_file(request):
 	try:
-		user = User.objects.get(identifier=request.POST.get('identifier'), code = request.POST.get('code'))
+		user = User.objects.get(username = request.user)
 	except User.DoesNotExist:
-		user = User(identifier = request.POST.get('identifier'), code = request.POST.get('code'))
+		user = User.objects.create_user(username = request.POST.get('username'), password = request.POST.get('password'))
 		user.save()
 	finally:
 		user_file = Content(user = user, file = request.FILES.get('filename'), date_time_delete = datetime.now() + timedelta(days = 180))
@@ -38,14 +31,12 @@ def save_user_file(request):
 
 def user_contents(request):
 	try:
-		user = User.objects.get(identifier=request.POST.get('identifier'), code = request.POST.get('code'))
+		user = User.objects.get(username = request.user)
 		contents = user.content_set.all()
 		return contents
 
 	except User.DoesNotExist:
-		return 'Files_not_exist'
-		#user = User(identifier = request.POST.get('identifier'), code = request.POST.get('code'))
-		#user.save()
+		return  None
 
 def user_content(user_id, content_id):
 	try:
@@ -54,5 +45,10 @@ def user_content(user_id, content_id):
 		return content
 
 	except User.DoesNotExist:
-		pass
+		return None
+
+def create_user(request):
+	user = User.objects.create_user(username = request.POST.get('username'), password = request.POST.get('password'))
+	user.save()
+	return user
 
